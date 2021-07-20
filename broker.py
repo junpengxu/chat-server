acc7f06941331f26835ffd996a84169d1eb0b672
junpengxu <weakeexu@gmai.com>
2021-07-21 01:40:06 +0800

    feat: update message structure

---
+++ broker.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/7/21 12:12 上午
+# @Author  : xu.junpeng
+
---
+++ serializable.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/7/21 1:08 上午
+# @Author  : xu.junpeng
+
